ac53c7e2e864eb5fe8814c865198a8fa01d544e1
naka2005 <103171123+naka2005@users.noreply.github.com>
2025-12-11 15:41:38 +0900

    selecter追加 注文削除

--- models/shift.py
+++ models/shift.py
@@ -1,11 +1,15 @@
-from peewee import Model, CharField, DateField, IntegerField
+from peewee import Model, CharField, DateField, IntegerField, ForeignKeyField
 from .db import db
+from .time import Time
+from .user import User
+from .product import Product
 
 
 class Shift(Model):
-    name = CharField()
-    workplace = IntegerField()
+    user = ForeignKeyField(User, backref="shifts")
+    product = ForeignKeyField(Product, backref="shifts")
     date = DateField()
+    time = ForeignKeyField(Time, backref="shifts")
 
     class Meta:
         database = db
